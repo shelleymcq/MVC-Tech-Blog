4dfc2641a003d0ea3f4be51ee75585dc2d87fc3e
Shelley M <81432121+shelleymcq@users.noreply.github.com>
2021-07-28 13:55:05 -0400

    added more styling and updated all variables to match blog jargon

--- models/Blog.js
+++ models/Blog.js
@@ -11,11 +11,11 @@ Blog.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    name: {
+    title: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    description: {
+    blog_text: {
       type: DataTypes.STRING,
     },
     date_created: {
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,13 +1,13 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#blog-name').value.trim();
-  const description = document.querySelector('#blog-desc').value.trim();
+  const title = document.querySelector('#blog-title').value.trim();
+  const blog_text = document.querySelector('#blog-text').value.trim();
 
-  if (name && description) {
+  if (title && blog_text) {
     const response = await fetch(`/api/blogs`, {
       method: 'POST',
-      body: JSON.stringify({ name, description }),
+      body: JSON.stringify({ title, blog_text }),
       headers: {
         'Content-Type': 'application/json',
       },

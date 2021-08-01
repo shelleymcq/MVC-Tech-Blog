8459e9152762cec575cc4ec87086f4035fa8fcc6
Shelley M <81432121+shelleymcq@users.noreply.github.com>
2021-08-01 11:06:39 -0400

    Revert "building comment functionality"
    This reverts commit c7267112fb251e5eecceb70b221a3b4e63d847de.

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
 const User = require('./User');
 const Blog = require('./Blog');
-const Comment = require('./Comment');
 
 User.hasMany(Blog, {
   foreignKey: 'user_id',
@@ -11,20 +10,4 @@ Blog.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-User.hasMany(Comment, {
-  foreignKey: 'user_id'
-});
-
-Blog.hasMany(Comment, {
-  foreignKey: 'blog_id'
-});
-
-Comment.belongsTo(Blog, {
-  foreignKey: 'blog_id'
-})
-
-Comment.belongsTo(User, {
-  foreignKey: 'user_id'
-})
-
-module.exports = { User, Blog, Comment };
+module.exports = { User, Blog };
